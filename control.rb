a84c3c129f19c82440d3ecdab59d6690baac6e9a
brandonricharda <brandonricharda@gmail.com>
2020-04-25 19:11:58 -0400

    Final commit of control file.

--- control.rb
+++ control.rb
@@ -1,9 +1,11 @@
 def gameSelection
     flag = false
     puts ""
+    puts "---"
     puts "If you would like to crack the computer's code, type 'FIRST OPTION' in all caps."
     puts ""
     puts "If you would like to select a code and have the computer crack it, type 'SECOND OPTION' in all caps."
+    puts "---"
     puts ""
     while !flag
         choice = gets.chomp
